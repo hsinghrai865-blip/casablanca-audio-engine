960676d8740934a7bc29861311cf6c2879092be0
hsinghrai865-blip <h.singhrai865@gmail.com>
2026-03-11 18:45:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -106,4 +106,4 @@ async def analyze(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
